96ed6430240de611204087d8fdf6d759617593dd
vmeset <vmeset@gmail.com>
2019-06-21 15:25:55 +0300

    Develop App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="head">
+       <img src="https://st3.depositphotos.com/6203808/16510/v/1600/depositphotos_165103386-stock-illustration-vintage-typography-design-with-ice.jpg" />
       </header>
+      <nav className="navigator">
+       <div>
+        Profile
+       </div>
+       <div>
+        Messages
+       </div>
+       <div>
+        Settings
+       </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="http://www.vitalsigns.graphics/wp-content/uploads/2018/09/badboy_lg-e1447284886415-600x328.jpg"/>
+        </div>
+      </div>
     </div>
   );
 }
