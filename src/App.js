a76138e08079864c83df3fc2476e9d505e17c457
vmeset <vmeset@gmail.com>
2019-06-23 14:15:24 +0300

    Create/rename/transfer Content/Profile

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import Head from "./components/header.js";
 import Navigator from "./components/navigator.js";
+import Profile from "./components/profile.js"
 
 function App() {
   return (
     <div className="wrapper">
       <Head />
       <Navigator />
-
-      <div className="content">
-        <div>
-          <img src="http://www.vitalsigns.graphics/wp-content/uploads/2018/09/badboy_lg-e1447284886415-600x328.jpg"/>
-        </div>
-      </div>
+      <Profile />
     </div>
   );
 }
---
+++ src/components/profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+			<div className="profile">
+				<div>
+					<img src="http://www.vitalsigns.graphics/wp-content/uploads/2018/09/badboy_lg-e1447284886415-600x328.jpg"/>
+				</div>
+			</div>
+		)
+}
+
+export default Profile;
