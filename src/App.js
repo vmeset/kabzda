6004e4df838b82ff8c5076fa124472fbb326dd6f
vmeset <vmeset@gmail.com>
2019-06-23 13:38:26 +0300

    Create/transfer Navigator component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
 import Head from "./components/header.js";
+import Navigator from "./components/navigator.js";
 
 function App() {
   return (
     <div className="wrapper">
       <Head />
-      <nav className="navigator">
-       <div>
-        Profile
-       </div>
-       <div>
-        Messages
-       </div>
-       <div>
-        Settings
-       </div>
-      </nav>
+      <Navigator />
+
       <div className="content">
         <div>
           <img src="http://www.vitalsigns.graphics/wp-content/uploads/2018/09/badboy_lg-e1447284886415-600x328.jpg"/>
---
+++ src/components/navigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navigator = () => {
+	return (
+		<nav className="navigator">
+			<div>
+				Profile
+			</div>
+			<div>
+				Messages
+			</div>
+			<div>
+				Settings
+			</div>
+		</nav>
+	)
+}
+
+export default Navigator;
