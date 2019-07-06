77ef6ece7c07309e63ff26d3452c45dfd0162705
vmeset <vmeset@gmail.com>
2019-07-06 14:08:49 +0300

    Added NavLinks to messages

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import styles from './messages.module.css'
+import {NavLink} from 'react-router-dom'
 
 const Messages = () => {
 	return (
 		<div className={styles.dialogs}>
 			<div className={styles.dialogItems}>
 				<div className={styles.dialog}>
-					John
+					<NavLink to="/messages/1">John</NavLink>
 				</div>
 				<div className={styles.dialog}>
-					Pierre
+					<NavLink to="/messages/2">Pierre</NavLink>
 				</div>
 				<div className={styles.dialog}>
-					Jorhe
+					<NavLink to="/messages/3">Jorhe</NavLink>
 				</div>
 			</div>
 			<div className={styles.messages}>
