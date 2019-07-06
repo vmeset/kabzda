271b9323ad44fb961036b164102b08261de8830a
vmeset <vmeset@gmail.com>
2019-07-06 10:12:13 +0300

    Modify messages component

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import styles from './messages.module.css'
 
 const Messages = () => {
 	return (
-		<div>
-			The message
+		<div className={styles.dialogs}>
+			<div className={styles.dialogItems}>
+				<div className={styles.dialog}>
+					John
+				</div>
+				<div className={styles.dialog}>
+					Pierre
+				</div>
+				<div className={styles.dialog}>
+					Jorhe
+				</div>
+			</div>
+			<div className={styles.messages}>
+				<div className={styles.message}>
+					hi
+				</div>
+				<div className={styles.message}>
+					salut
+				</div>
+				<div className={styles.message}>
+					hola
+				</div>
+			</div>
+
 		</div>
 	)
 }
--- src/components/navigator/navigator.js
+++ src/components/navigator/navigator.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from './navigator.module.css'
+import {NavLink} from 'react-router-dom'
 
 const Navigator = () => {
 	return (
 		<nav className={styles.navigator}>
 		<div className={styles.item}>
-			<a href='/profile'>Profile</a>
+			<NavLink to='/profile'>Profile</NavLink>
 		</div>
 		<div className={`${styles.item} ${styles.active}`}>
-			<a href='/messages'>Messages</a>
+			<NavLink to='/messages'>Messages</NavLink>
 		</div>
 		<div>
 			Settings
