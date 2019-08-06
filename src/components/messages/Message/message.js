8ab53d07eeebf042d8c8ba9268bbcdaf01ff96a6
vmeset <vmeset@gmail.com>
2019-08-06 11:58:32 +0300

    Add textarea blocks for Post and Message

--- src/components/messages/Message/message.js
+++ src/components/messages/Message/message.js
@@ -2,9 +2,22 @@ import React from 'react'
 import styles from './../messages.module.css'
 
 const Message = (props) => {
+
+	let textareaRef = React.createRef()
+
+	let newMessage = (props) => {
+
+		let Text = textareaRef.current.value
+
+	}
+
 	return (
 		<div className={styles.message}>
-			{props.message}
+			<div>
+				{props.message}
+				<textarea ref={textareaRef}></textarea>
+				<button onClick={newMessage}>Send</button>
+			</div>
 		</div>
 	)
 }
--- src/components/profile/myPosts/myPosts.js
+++ src/components/profile/myPosts/myPosts.js
@@ -6,8 +6,23 @@ const MyPosts = (props) => {
 
 	let postsElements = props.posts.map((element) => <Post message={element.message} likes={element.likes} />)
 
+	let addPost = () => {
+		let text = inputText.current.value
+	}
+
+	let inputText = React.createRef()
+
 	return (
 		<div>
+			<div>
+				<textarea ref={inputText}>
+
+				</textarea>
+			</div>
+			<div>
+				<button onClick={addPost}>Add</button>
+			</div>
+
 			{postsElements}
 		</div>
 		)
