fff21527a7230928102cd256b583673410cbbceb
vmeset <vmeset@gmail.com>
2020-01-18 12:27:58 +0300

    test commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import styles from './header.module.css'
 const Head = () => {
 	return (
 		<header className={styles.head}>
+			test
 			<img src="https://st3.depositphotos.com/6203808/16510/v/1600/depositphotos_165103386-stock-illustration-vintage-typography-design-with-ice.jpg" />
 		</header>
 	)
